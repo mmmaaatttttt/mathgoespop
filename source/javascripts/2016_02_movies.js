d53bd3ccdca94727702af830b9a2495b69ad6f16
Matt Lane <lane.matthew@gmail.com>
2016-01-30 20:56:54 -0800

    add year slider

--- source/javascripts/2016_02_movies.js
+++ source/javascripts/2016_02_movies.js
@@ -7,7 +7,9 @@
   var curYear = 2015;
   var curSeason = "summer";
   var dotRadius = 5;
-  var curMovies, xScale, yScale, revisedMovies;
+  var $yearSlider = $("#yearSlider");
+  var $yearText = $(".year");
+  var xScale, yScale, revisedMovies;
 
   d3.json('/javascripts/2016_movies.json', function(data) {
 
@@ -19,31 +21,47 @@
       return movie;
     });
     
-    // take all movies, discard all but current movie and season,
-    curMovies = revisedMovies.filter(function(movie) { 
-      return movie.releaseYear === curYear.toString() && movie.season === curSeason; 
-    });
-
     // create scales
     xScale = d3.scale.linear()
-                     .domain([0, d3.max(curMovies, function(movie) { return movie.weeklyGrosses.length })])
+                     .domain([0, d3.max(revisedMovies, function(movie) { return movie.weeklyGrosses.length })])
                      .range([dotRadius, svgWidth - dotRadius]);
     yScale = d3.scale.linear()
-                     .domain([0, d3.max(curMovies, function(movie) { return movie.total })])
+                     .domain([0, d3.max(revisedMovies, function(movie) { return movie.total })])
                      .range([parseInt(svgHeight.slice(0,3)) - dotRadius, dotRadius]); 
 
-    curMovies.forEach(function(movie, idx) {
-      console.log(movie)
-      svg.selectAll('circle.' + curSeason + '.year' + curYear + '.movie' + idx)
+    revisedMovies.forEach(function(movie, idx) {
+      svg.selectAll('circle.' + movie.season + '.year' + movie.releaseYear + '.movie' + idx)
        .data(movie.weeklyGrosses)
        .enter()
        .append('circle')
        .attr('cx', function(d, i) { return xScale(i); })
        .attr('cy', function(d) { return yScale(d); })
-       .attr('r', dotRadius);
+       .attr('r', dotRadius)
+       .classed(movie.season + ' year' + movie.releaseYear + ' movie' + idx, true);
     });
     
-    // console.log(revisedMovies);
+    showData(curSeason, curYear);
+
   });
+
+  var yearSlider = new ScrubberView(); 
+  yearSlider.min(1986).max(2015).step(1).value(2015);
+  // yearSlider.thumb.style.background = 
+  // yearSlider.thumb.style.borderColor = 
+  yearSlider.onValueChanged = function(v) {
+    clearData();
+    curYear = v;
+    $yearText.text(v);
+    showData(curSeason, curYear);
+  };
+  $yearSlider.append(yearSlider.elt); 
+
+  function clearData() {
+    d3.selectAll('circle').classed('visible', false);
+  }
+
+  function showData(season, year) {
+    d3.selectAll('circle.' + season + '.year' + year).classed('visible', true);
+  }
 
 });
