318c8a7384519df172e9aa9495001dc0c28163bf
Matt Lane <lane.matthew@gmail.com>
2019-08-31 08:00:01 -0700

    adding links to mattlane.us

--- renderer.rb
+++ renderer.rb
@@ -3,7 +3,7 @@ require "middleman-core/renderers/redcarpet"
 class MGPRenderer < Middleman::Renderers::MiddlemanRedcarpetHTML
 
   def link(link, title, content)
-    "<a href='#{link}' target='_blank'>#{content}</a>"
+    "<a href='#{link}' target='_blank' rel='noopener noreferrer'>#{content}</a>"
   end
 
 end
